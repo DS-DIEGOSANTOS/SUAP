bcf610cb19497b9697a953ade700ff2702493be7
DIEGO SANTOS DOS SANTOS <84682022+DS-DIEGOSANTOS@users.noreply.github.com>
2021-06-27 00:15:33 -0300

    criando icon suap no menu

---
+++ SUAP.c
@@ -0,0 +1,30 @@
+// BIBLIOTECA UTILIZADA
+#include <stdio.h>
+#include <stdio.h>
+#include <locale.h>
+
+int main (){
+    setlocale(LC_ALL,"ptb");
+
+    printf("  SSSSSSSSSSSSSSS    UUUUUUU     UUUUUUU               AAA                 PPPPPPPPPPPPPPPPP\n");
+    printf(" SS:::::::::::::::S  U:::::U     U:::::U             A:::::A               P::::::::::::::::PP\n");
+    printf("S:::::SSSSSS::::::S  U:::::U     U:::::U            A::AAA::A              P::::PPPPPPPPP::::PP\n");
+    printf("S:::::S     SSSSSSS  U:::::U     U:::::U           A::A   A::A             P::::P        P::::PP\n");
+    printf("S:::::S              U:::::U     U:::::U          A::A     A::A            P::::P        P::::PP\n");
+    printf("S:::::S              U:::::D     D:::::U         A::A       A::A           P::::P        P::::PP\n");
+    printf(" S::::SSSS           U:::::D     D:::::U        A::A         A::A          P::::PPPPPPPPP::::PP\n");
+    printf("  SS::::::SSSSS      U:::::D     D:::::U       A::A           A::A         P:::::::::::::::PP\n");
+    printf("    SSS::::::::SS    U:::::D     D:::::U      A::::AAAAAAAAAA:::::A        P::PPPPPPPPPPPP\n");
+    printf("       SSSSSS::::S   U:::::D     D:::::U     A:::::::::::::::::::::A       P::P\n");
+    printf("            S:::::S  U:::::D     D:::::U    A:::::::::::::::::::::::A      P::P\n");
+    printf("            S:::::S  U::::::U   U::::::U   A::::: AAAAAAAAAAAA:::::::A     P::P\n");
+    printf("SSSSSSS     S:::::S  U:::::::UUU:::::::U  A:::::A              A::::::A    P::P\n");
+    printf("S::::::SSSSSS:::::S   UU:::::::::::::UU  A:::::A                A::::::A   P::P\n");
+    printf("S:::::::::::::::SS     UU:::::::::UU    A:::::A                  A::::::A  P::P\n");
+    printf(" SSSSSSSSSSSSSSS         UUUUUUUUU      AAAAAA                     AAAAA   PPPP\n");
+
+    printf("\n\n\n                      ..:: Sejam bem-vindos ao SUAP-Sistena Academico! ::.. ");
+
+system('pause');
+return 0;
+}
